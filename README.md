0567be036404ecde41b45f4ca90a730c9a7cb95b
Lucian Ralea <lucian.ralea@gmail.com>
2026-09-24 08:50:40 +0200

    Task 1-3 done

--- README.md
+++ README.md
@@ -3,5 +3,24 @@
 Requirements: Typescript and Express packages plus the Zod package 
 Install Zod with: "npm i Zod"
 
-The first thing that appears once the banking terminal starts is the main menu (see image below):
-![Banking terminal - main menu](images/mainmenu.png "Main menu")
+**Task 1: Test the server**
+Method: GET
+Route "/ping"
+Result: The server answers with "Pong (the server is up and running)
+![Task-1](/images/task1.png)
+
+
+**Task 2: Fetch user and country**
+Method: GET
+Route "/random-person"
+Result: The server answers with information about user name and country
+![Task-2](/images/task2.png)
+
+**Task 3: Simulate adding a user**
+Method: POST
+Route "/users"
+Result: If successfull, the server confirms that a new user has been added and what the user data was.
+![Task-3](/images/task3.png)
+
+If the data is corrupted somehow (in this case age was sent as a string insted of a number), the response could look similar to the following image: ![Task-3](/images/task3-error-in-age.png)
+
--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,71 @@
 import { z } from "zod";
+import express from "express";
 
-const usernameSchema = z.string().min(3).max(10);
+const app = express();
+const PORT = 3000;
 
+app.use(express.json());
 
-const validateUsername = usernameSchema.safeParse("Jo"); 
-if (!validateUsername.success) {
-  console.error(validateUsername.error);
-}
-else {
-  console.log("It's all fine", validateUsername.data);
-}
+const userSchema = z.object({
+  name: z.string().min(3).max(12),
+  age: z.number()
+  .min(18, { message: "Age must be at least 18" })
+  .max(100, { message: "Age must be less than 100" })
+  .optional()
+  .default(28),
+  email: z.email().toLowerCase(),
+});
+
+const randomUserResponseSchema = z.object({
+  results: z.array(
+    z.object({
+      name: z.object({
+        first: z.string(),
+        last: z.string()
+      }),
+      location: z.object({
+        country: z.string()
+      })
+    })
+  )
+});
+
+app.get("/random-person", async (req, res) => {
+  try {
+    const response = await fetch("https://randomuser.me/api");
+    const data = await response.json();
+
+    const validatedRandomUser = randomUserResponseSchema.safeParse(data);
+    if (!validatedRandomUser.success) {
+      return res.status(500).json({
+        error: "Invalid users data from Randomuser API",
+        details: validatedRandomUser.error,
+      });
+    }
+
+    const randomUser = validatedRandomUser.data.results[0];
+    return res.json({
+      name: `Name: ${randomUser.name.first} ${randomUser.name.last}`,
+      country: `Country: ${randomUser.location.country}`,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch random user data" });
+  }
+});
+
+app.get("/ping", async (req, res) => {
+    res.status(200).json({ message: "Pong (the server is up and running)" });
+  });
+
+  app.post("/users", (req, res) => {
+  const validatedNewUser = userSchema.safeParse(req.body);
+  if (!validatedNewUser.success) {
+    return res.status(400).json({ error: "Invalid users data", details: validatedNewUser.error });
+  } else {
+    res.status(201).json({ message: "User created successfully", user: validatedNewUser.data });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
